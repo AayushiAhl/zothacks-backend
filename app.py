1b6c98b0f2ee158321a7884dc30fe371a053cc88
Ian Dai <ianbbdai@gmail.com>
2024-02-21 11:49:01 -0800

    Basic functionality works for getting any api call

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template
-from requests.exceptions import HTTPError
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
-
-# Replace YOUR-API-KEY with your API key in order for the API call to work
-key_header = {"x-api-key": "YOUR-API-KEY"}
 
 # The API url that you are calling
 # For more info, see https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
@@ -15,42 +11,25 @@ api_url = "https://api.thecatapi.com/v1/images/search?has_breeds=1"
 @app.route("/")
 def home():
     """Called when navigating to the home page"""
-    # Try to call API and return an error message if any errors occur
-    try:
-        cat_info = acquire_cat()
-    except HTTPError as err:
-        return err.response.text
-
-    # Returns a rendered html file with the name, description, and img_url args
-    # The args are used for the Jinja templating in home.html
-    return render_template(
-        "home.html",
-        name=cat_info["name"],
-        description=cat_info["description"],
-        img_url=cat_info["img_url"],
-    )
+    return render_template("home.html")
 
 
-@app.route("/acquire-cat")
-def acquire_cat():
-    # API call using requests library and get method
-    raw_resp = requests.get(
-        api_url,
-        headers=key_header,
-    )
-    raw_resp.raise_for_status()
+@app.route("/fetch-api", methods=["POST"])
+def fetch_api():
+    if request.method == "POST":
+        api_url = request.form["api-url"]
 
-    # Read API response into a Python dictionary
-    resp = raw_resp.json()
+        # API call using requests library and get method
+        try:
+            raw_resp = requests.get(api_url)
+            raw_resp.raise_for_status()
+        except:
+            return {"error": "Ensure your URL and API key is correct."}
 
-    # Extract exact dictionary from API response
-    cat_info = resp[0]["breeds"][0]
+        # Read API response into a Python dictionary
+        resp = raw_resp.json()
 
-    # If the caller is outside Python, Flask will return this Python dictionary
-    # as a serialized JSON string.
-    # This will be useful, for example, for Javascript code calling this endpoint
-    return {
-        "name": cat_info["name"],
-        "description": cat_info["description"],
-        "img_url": resp[0]["url"],
-    }
+        # If the caller is outside Python, Flask will return this Python dictionary
+        # as a serialized JSON string.
+        # This will be useful, for example, for Javascript code calling this endpoint
+        return resp
